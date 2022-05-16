766e67d42a03072a2f1d88b5e1673349e04dd314
Farkhan Azmi <fliazmi@gmail.com>
2022-05-16 09:45:07 +0700

    tweaking

--- api/index.js
+++ api/index.js
@@ -9,46 +9,51 @@ class Request {
     this.successHandler = handler?.successHandler;
   };
 
-  create = async ({ url, body, headers, method }) => {
-    const token = await getItem("token").catch(() => false);
-    const options = {
-      method,
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        ...(token && { Authorization: `Bearer ${token}` }),
-        ...(headers && headers),
-      },
-    };
+  create = ({ url, body, headers, method }) =>
+    new Promise(async (resolve, reject) => {
+      const token = await getItem("token").catch(() => false);
+      const options = {
+        method,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          ...(token && { Authorization: `Bearer ${token}` }),
+          ...(headers && headers),
+        },
+      };
 
-    const wrapperLog = (response, error = false) =>
-      logger(
-        `\n🚀 Url::${url}`,
-        `\n🗿 Body::${JSON.stringify(body || {})}`,
-        `\n🚧 Option::${JSON.stringify(options)}`,
-        `\n${error ? "💀" : "🦄"} Response::${JSON.stringify(response)}`
-      );
+      const wrapperLog = (response, error = false) =>
+        logger(
+          `\n🚀 Url::${url}`,
+          `\n🗿 Body::${JSON.stringify(body || {})}`,
+          `\n🚧 Option::${JSON.stringify(options)}`,
+          `\n${error ? "💀" : "🦄"} Response::${JSON.stringify(response)}`
+        );
 
-    const onSuccess = (res) => {
-      this.successHandler(res);
-      const response = res?.data || res;
-      wrapperLog(response);
-      return Promise.resolve(response);
-    };
+      const onSuccess = (res) => {
+        this.successHandler(res);
+        const response = res?.data || res;
+        wrapperLog(response);
+        resolve(response);
+      };
 
-    const onError = (err) => {
-      this.errorHandler(err);
-      const response = err?.response?.data || err?.response || err;
-      wrapperLog(response, true);
-      return Promise.reject(response);
-    };
+      const onError = (err) => {
+        this.errorHandler(err);
+        const response = err?.response?.data || err?.response || err;
+        wrapperLog(response);
+        reject(response);
+      };
 
-    return fetch(url, { ...options, body: JSON.stringify(body) })
-      .then(async (resp) => {
-        let res = await resp.json();
-        resp.ok ? onSuccess(res) : onError(res);
-      })
-      .catch((err) => onError(err));
-  };
+      try {
+        let response = await fetch(url, {
+          ...options,
+          body: JSON.stringify(body),
+        });
+        let res = await response.json();
+        response.ok ? onSuccess(res) : onError(res);
+      } catch (error) {
+        onError(error);
+      }
+    });
 }
 export default Request;
