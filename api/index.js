bef1b1727d2cbdeadb273a9e09bec99fbec5a289
Farkhan Azmi <fliazmi@gmail.com>
2022-06-04 23:06:12 +0700

    update api

--- api/index.js
+++ api/index.js
@@ -9,14 +9,19 @@ class Request {
     this.successHandler = handler?.successHandler;
   };
 
-  create = ({ url, body, headers, method, disableLog = false }) =>
+  create = ({ url, payload, headers, method, disableLog = false }) =>
     new Promise(async (resolve, reject) => {
+      let methodWithoutBody = ["GET", "HEAD"];
+      let body = new FormData();
+      payload &&
+        Object.keys(payload).forEach((key) => body.append(key, payload[key]));
+
       const token = await getItem("token").catch(() => false);
       const options = {
         method,
         headers: {
           Accept: "application/json",
-          "Content-Type": "application/json",
+          "Content-Type": "multipart/form-data",
           ...(token && { Authorization: `Bearer ${token}` }),
           ...(headers && headers),
         },
@@ -25,7 +30,8 @@ class Request {
       const wrapperLog = ({ response, error = false }) =>
         logger(
           `\n🚀 Url::${url}`,
-          `\n🗿 Body::${JSON.stringify(body || {})}`,
+          !methodWithoutBody.includes(method) &&
+            `\n🗿 Body::${JSON.stringify(body || {})}`,
           `\n🚧 Option::${JSON.stringify(options)}`,
           `\n${error ? "💀" : "🦄"} Response::${JSON.stringify(response)}`
         );
@@ -39,7 +45,7 @@ class Request {
       try {
         let response = await fetch(url, {
           ...options,
-          body: JSON.stringify(body),
+          ...(!methodWithoutBody.includes(method) && { body }),
         });
         let res = await response.json();
         res = {
@@ -48,8 +54,10 @@ class Request {
         };
         on({ response: res, error: !response.ok });
       } catch (error) {
+        console.log("error", JSON.stringify(error));
         on({ response: error, error: true });
       }
     });
 }
+
 export default Request;
